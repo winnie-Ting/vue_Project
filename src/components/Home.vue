83eb015ee03f3c000e78c7349ecda32f637a0ef1
winnieTing <254106056@qq.com>
2020-06-24 18:23:17 +0800

    login

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,139 @@
 <template>
-  <div>
-    Home组件
-    <br />
-    <el-button type="info" @click="logout">退出登录</el-button>
-  </div>
+  <el-container class="home_container">
+    <!--头部区域--->
+    <el-header>
+      <div>
+        <img src="../assets/images/logo_1.jpg" alt="logo" />
+        <span>后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <!--侧边栏-->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <!-- 左侧菜单 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="true"
+          :default-active="activePath"
+        >
+          <div class="toogle-collaps" @click="toggleCollapse">|||</div>
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + '' " v-for="item in menulist" :key="item.id">
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <!-- index充当 router中的path -->
+            <el-menu-item
+              :index="'/'+subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/'+subItem.path)"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!--主体区域-->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 左侧菜单数据
+      menulist: [],
+      iconsObj: {
+        '125': 'iconfont icon-mine_fill',
+        '103': 'iconfont icon-manage',
+        '101': 'iconfont icon-commodity',
+        '102': 'iconfont icon-order',
+        '145': 'iconfont icon-dataanalysis'
+      },
+      isCollapse: false,
+      activePath: ''
+    }
+  },
+  // 生命周期钩子函数
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePathName')
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      console.log(res)
+      this.menulist = res.data
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePathName', activePath)
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.home_container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 0;
+  color: #fff;
+  font-size: 30px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 20px;
+    }
+    img {
+      border-radius: 50%;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: 0;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.toogle-collaps {
+  color: #fff;
+  background-color: #4a5064;
+  letter-spacing: 1px;
+  line-height: 24px;
+  text-align: center;
+  cursor: pointer;
+}
 </style>
