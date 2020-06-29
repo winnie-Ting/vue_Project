3070ef30a378a1c877edcd1d0773d6eee7e44a79
winnieTing <254106056@qq.com>
2020-06-29 14:49:41 +0800

    goods_params

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import {
   Aside,
+  Alert,
   Button,
   Breadcrumb,
   BreadcrumbItem,
@@ -29,7 +30,9 @@ import {
   TableColumn,
   Tooltip,
   Tag,
-  Tree
+  Tree,
+  Tabs,
+  TabPane
 } from 'element-ui'
 
 Vue.use(Button)
@@ -60,5 +63,8 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
 Vue.prototype.$Message = Message
 Vue.prototype.$confirm = MessageBox.confirm
